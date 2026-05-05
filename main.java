c27465cbbd571a1d08401ae8e8553097c7f3d45f
HaxTwoWater <louis.jeannot@ynov.com>
2026-05-05 21:00:38 +0200

    feat(main): added the simu instance starting

--- main.java
+++ main.java
@@ -1,5 +1,6 @@
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Simulator simulator = Simulator.getInstance();
+        simulator.start();
     }
 }
